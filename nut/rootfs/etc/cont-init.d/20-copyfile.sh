e0ccdddc8a87ea6cfd1a30f71601be305c30538b
Gianpiero <gpammendola@yahoo.it>
2019-09-12 23:30:53 +0200

    Create 20-copyfile.sh

---
+++ nut/rootfs/etc/cont-init.d/20-copyfile.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Community Hass.io Add-ons: Network UPS Tools
+# Ensures the configuration exists
+# ==============================================================================
+declare -a CONF_FILES=("nut.conf" "ups.conf" "upsd.conf" "upsmon.conf" "upssched.conf")
+
+#set -e
+# Ensure configuration exists
+if ! bashio::fs.directory_exists "/share/nut"; then
+    mkdir -p /share/nut \
+        || bashio::exit.nok "Failed to create NUT configuration directory"
+fi
+
+# Copy configuration file on share folder
+bashio::log.info "Copying user config files on share folder"
+for file in "${CONF_FILES[@]}"; do
+    cp "/etc/nut/${file}" /share/nut
+done
+
+chmod -R 777 /config/nut/*
